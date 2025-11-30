6c87ee631105c16e138f039a21cc16ff7676ddd0
12sor21 <make.a.game100@gmail.com>
2025-11-30 18:14:26 +0100

    Registration + Password change changed login.dart and created forgot_pw_page.dart

--- lib/auth/forgot_pw_page.dart
+++ lib/auth/forgot_pw_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'login.dart';
 
 class ForgotPasswordPage extends StatefulWidget {
   const ForgotPasswordPage({super.key});
