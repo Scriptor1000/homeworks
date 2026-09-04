26c35a53a9ad587dd681ff679956910f2c546bfc
Max <maxruetti@gmail.com>
2026-09-04 16:20:38 +0200

    feat: switch to google crashlytics

--- lib/views/account.dart
+++ lib/views/account.dart
@@ -11,13 +11,45 @@ class AccountView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Konto')),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.all(16.0),
           child: Column(
             children: [
               UserContainer(),
               // Hier kann später weiterer Inhalt für die Account-Seite eingefügt werden
+              TextButton(
+                onPressed: () {
+                  // Show a dialog to confirm the crash test
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text('Test Crash'),
+                        content: const Text(
+                          'Möchten Sie die App wirklich zum Absturz bringen?',
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop(); // Close the dialog
+                            },
+                            child: const Text('Abbrechen'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                              throw Exception('Test Crash ausgelöst');
+                            },
+                            child: const Text('Ja, Crash auslösen'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: Text('Test Crash'),
+              ),
             ],
           ),
         ),
