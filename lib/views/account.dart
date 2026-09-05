0af5deccf54dc7d99a05a8c03b8ffa49b08c3fc6
Max <maxruetti@gmail.com>
2026-09-05 17:25:30 +0200

    fix: remove test-crash button

--- lib/views/account.dart
+++ lib/views/account.dart
@@ -18,38 +18,6 @@ class AccountView extends StatelessWidget {
             children: [
               UserContainer(),
               // Hier kann später weiterer Inhalt für die Account-Seite eingefügt werden
-              TextButton(
-                onPressed: () {
-                  // Show a dialog to confirm the crash test
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: const Text('Test Crash'),
-                        content: const Text(
-                          'Möchten Sie die App wirklich zum Absturz bringen?',
-                        ),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop(); // Close the dialog
-                            },
-                            child: const Text('Abbrechen'),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                              throw Exception('Test Crash ausgelöst');
-                            },
-                            child: const Text('Ja, Crash auslösen'),
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                },
-                child: Text('Test Crash'),
-              ),
             ],
           ),
         ),
