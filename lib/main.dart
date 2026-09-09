495671ece02b554aeb24de3d744c4fdd11d4b719
Max <maxruetti@gmail.com>
2026-09-09 21:49:55 +0200

    feat: activate firebase-app-check for api security

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
@@ -40,6 +41,13 @@ void main() async {
   );
 
   await FirebaseAnalytics.instance.setAnalyticsCollectionEnabled(kReleaseMode);
+
+  await FirebaseAppCheck.instance.activate(
+    providerAndroid: kDebugMode
+        ? AndroidDebugProvider()
+        : AndroidPlayIntegrityProvider(),
+    providerApple: AppleAppAttestProvider(),
+  );
 
   FlutterNativeSplash.remove();
   if (kReleaseMode) {
